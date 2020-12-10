dfcbafffbee0f2dec3c5b4afd8130960cb4a0ea8
SaeryDev <sbaldin.dev@gmail.com>
2020-12-10 15:22:13 +0100

    Layout: media queries changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
